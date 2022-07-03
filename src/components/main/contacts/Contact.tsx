742566b67bccd0db90e76fee229dedd77c16a74d
Saransh Saini <saranshsss99@gmail.com>
2022-07-03 18:06:07 +0530

    contacts layout updated

--- src/components/main/contacts/Contact.tsx
+++ src/components/main/contacts/Contact.tsx
@@ -11,31 +11,31 @@ const Contact: FC<ContactType> = ({
 }) => {
   const { dispatch } = useGLobalContext();
   return (
-    <article className="border-b-2 pb-1">
-      <ul className="grid grid-cols-5 px-2 py-1 items-baseline">
-        <li>{list_number}</li>
-        <li>{name}</li>
-        <li>{email}</li>
-        <li>{phone_number}</li>
-        <li className="flex gap-1 ">
-          <button
-            className="bg-blue-600 rounded-sm text-white px-2 py-0.5"
-            onClick={() => {
-              dispatch({ type: "Set_Edit_Field_Id", payload: id });
-            }}
-          >
-            Edit
-          </button>
-          <button
-            className="bg-red-600 rounded-sm text-white px-2 py-0.5"
-            onClick={() => {
-              dispatch({ type: "Delete_Contact", payload: id });
-            }}
-          >
-            Delete
-          </button>
-        </li>
-      </ul>
+    <article className="flex p-4 pt-1 gap-x-4 border-b-2 ">
+      <p>{list_number}</p>
+      <div className="flex flex-col gap-y-1.5 ">
+        <p>{name}</p>
+        <p>{email}</p>
+        <p>{phone_number}</p>
+      </div>
+      <div className="flex flex-col gap-1 ml-auto justify-evenly">
+        <button
+          className="bg-blue-600 rounded-sm text-white px-2 py-0.5"
+          onClick={() => {
+            dispatch({ type: "Set_Edit_Field_Id", payload: id });
+          }}
+        >
+          Edit
+        </button>
+        <button
+          className="bg-red-600 rounded-sm text-white px-2 py-0.5"
+          onClick={() => {
+            dispatch({ type: "Delete_Contact", payload: id });
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </article>
   );
 };
